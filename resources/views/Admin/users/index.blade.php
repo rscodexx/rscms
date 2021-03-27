8cdb6568d641531a44e3839a261cb0d4e8b89487
Rafael Santos <raffrenan@gmail.com>
2021-03-27 11:38:01 -0300

    Início da página meus usuários.

---
+++ resources/views/Admin/users/index.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+
+@section('title', 'Usuarios')
+
+@section('content_header')
+    <h1>Meus usuários.</h1>
+@endsection
+
+@section('content')
+
+    ..
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,6 @@ Route::prefix('painel')->group(function(){
 
     Route::post('logout', 'Admin\Auth\LoginController@logout')->name('logout');
 
+    Route::resource('users', 'Admin\UserController');
+
 });
