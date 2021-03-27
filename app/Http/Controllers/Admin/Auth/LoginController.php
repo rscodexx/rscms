fe52d6d64b141b88534bca981ccf1526088f37b0
Rafael Santos <raffrenan@gmail.com>
2021-03-27 10:27:34 -0300

    Criando função de login com validação.

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -43,11 +44,34 @@ class LoginController extends Controller
         return view('admin.login');
     }
 
-    public function authenticate(){
+    public function authenticate(Request $request){
+        $data = $request->only(['email', 'password', 'remember']);
+
+        $validator = $this->validator($data);
+
+        if($validator->fails()){
+            return redirect()->route('login')->withErrors($validator)->withInput();
+        }
+
+        if(Auth::attempt($data)){
+            return redirect()->route('admin');
+        } else {
+
+            $validator->errors()->add('password', 'E-mail e/ou senha incorreta.');
+
+            return redirect()->route('login')->withErrors($validator)->withInput();
+        }
     }
 
     public function logout(){
         Auth::logout();
         return redirect()->route('login');
+    }
+
+    protected function validator(array $data){
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max: 100'],
+            'password' => ['required', 'string', 'min:8']
+        ]);
     }
 }
