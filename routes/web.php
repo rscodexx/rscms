5684999bbe19ba11057ecbf70d5ba343eb5bfa23
Rafael Santos <raffrenan@gmail.com>
2021-03-27 10:00:30 -0300

    Subindo aplicação com configurações básicas.

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'Site\HomeController@index');
+
+Route::prefix('painel')->group(function(){
+
+    Route::get('/', 'Admin\HomeController@index')->name('admin');
+
+    Route::get('login', 'Admin\Auth\LoginController@index')->name('login');
+    Route::post('login', 'Admin\Auth\LoginController@authenticate');
+
+    Route::get('register', 'Admin\Auth\RegisterController@index')->name('register');
+    Route::post('register', 'Admin\Auth\RegisterController@register');
+
+});
