db970d4dfac7e74d3cad0d7bbcf0b3b6acb491ad
Rafael Santos <raffrenan@gmail.com>
2021-03-29 09:11:31 -0300

    Adicionando páginação.

--- resources/views/Admin/users/index.blade.php
+++ resources/views/Admin/users/index.blade.php
@@ -11,26 +11,34 @@
 
 @section('content')
 
-    <table class="table table-hover">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Ações</th>
-        </tr>
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>
+                                <a href="{{route('users.edit', [$user->id])}}" class="btn btn-sm btn-info">Editar</a>
+                                <a href="{{route('users.destroy', [$user->id])}}" class="btn btn-sm btn-danger">Excluir</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 
-        @foreach($users as $user)
-            <tr>
-                <td>{{$user->id}}</td>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-                <td>
-                    <a href="{{route('users.edit', [$user->id])}}" class="btn btn-sm btn-info">Editar</a>
-                    <a href="{{route('users.destroy', [$user->id])}}" class="btn btn-sm btn-danger">Excluir</a>
-                </td>
-            </tr>
-        @endforeach
-
-    </table>
+    {{$users->links()}}
 
 @endsection
