d12962e39ca178dceae238e6968d7660732c4006
Rafael Santos <raffrenan@gmail.com>
2021-03-30 19:57:48 -0300

    Adicionando site e configurações iniciais.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,8 @@
-@extends('site.layout')
+@extends('layouts.site')
 
-@section('title', 'TITULO DE TESTE')
+@section('title', $settings["title"])
 
 @section('content')
-
     <!-- slider_area_start -->
     <div class="slider_area">
         <div class="single_slider  d-flex align-items-center slider_bg_1">
@@ -11,8 +10,8 @@
                 <div class="row align-items-center">
                     <div class="col-xl-7 col-md-6">
                         <div class="slider_text ">
-                            <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >{{$front_config['title']}}</h3>
-                            <p class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">{{$front_config['subtitle']}}</p>
+                            <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >{{$settings["title"]}}</h3>
+                            <p class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">{{$settings["subtitle"]}}</p>
                             <div class="video_service_btn wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".1s">
                                 <a href="#" class="boxed-btn3">Get Start Now</a>
                             </div>
@@ -20,7 +19,7 @@
                     </div>
                     <div class="col-xl-5 col-md-6">
                         <div class="phone_thumb wow fadeInDown" data-wow-duration="1.1s" data-wow-delay=".2s">
-                            <img src="img/ilstrator/phone.png" alt="">
+                            <img src="{{asset('applab/img/ilstrator/phone.png')}}" alt="phone">
                         </div>
                     </div>
                 </div>
@@ -28,5 +27,4 @@
         </div>
     </div>
     <!-- slider_area_end -->
-
 @endsection
