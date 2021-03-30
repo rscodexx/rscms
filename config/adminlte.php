a0c446047c453623ac6bf0b5461a13f01e24279b
Rafael Santos <raffrenan@gmail.com>
2021-03-30 20:33:31 -0300

    Adicionando configurações finais.

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>RS</b>CMS',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '',
+    'logo_img' => 'vendor/adminlte/dist/img/RSCodeLogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'RS Code',
 
     /*
     |--------------------------------------------------------------------------
